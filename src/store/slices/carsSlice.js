c4c7f178614d10e988c04a24853657f5498070e9
Rabia-Rajput <rajput.rabia@outlook.com>
2024-07-10 12:01:18 +0200

    update carsSlice

---
+++ src/store/slices/carsSlice.js
@@ -0,0 +1,31 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import React from "react";
+
+const carsSlice = createSlice({
+  name: "cars",
+  initialState: {
+    searchTerm: "",
+    cars: [],
+  },
+  reducers: {
+    changeSearchTerm(state, action) {
+      state.searchTerm = action.payload;
+    },
+    addCar(state, action) {
+      state.cars.push({
+        name: action.payload.name,
+        cost: action.payload.cost,
+        id: nanoid(),
+      });
+    },
+    removeCar(state, action) {
+      //assumption that action.payload=== the id of the car we want to remove
+      const updated = state.cars.filter((car) => {
+        return car.id !== action.payload;
+      });
+      state.cars = updated;
+    },
+  },
+});
+export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions;
+export const carsReducer = carsSlice.reducer;
