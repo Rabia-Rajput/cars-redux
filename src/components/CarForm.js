c18e7918d7683be6d8b6e2724e5d19ecbb19c8a0
Rabia-Rajput <rajput.rabia@outlook.com>
2024-07-10 12:57:13 +0200

    update carform file

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,31 +1,56 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { changeName } from '../store/slices';
-
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { changeCost, changeName } from "../store/slices";
 
 const CarForm = () => {
-  
-  const dispatch= useDispatch();
- const name= useSelector((state)=>{
-    return state.form.name;
-  })
-  const handleChange=(e)=>{
-dispatch(changeName(e.target.value));
+  const dispatch = useDispatch();
+  const { name, cost } = useSelector((state) => {
+    return {
+      name: state.form.name,
+      cost: state.form.cost,
+    };
+  });
+  const handleNameChange = (e) => {
+    dispatch(changeName(e.target.value));
   };
+
+  const handleCostChange = (e) => {
+    const carCost = parseInt(e.target.value) || 0;
+    dispatch(changeCost(carCost));
+  };
+  const handleFormSubmit=(event)=>{
+    event.preventDefault();
+  }
   return (
-    <div className='car-form panel'>
-      <h4 className='subtitle is-3'>Add Car</h4>
-      <form>
-        <div className='field-group'> 
-          <div className='field'>
-            <label className='label'>Name</label>
-            <input className='input is-expanded' value={name} onChange={handleChange}/>
+    <div className="car-form panel">
+      <h4 className="subtitle is-3">Add Car</h4>
+      <form onSubmit={handleFormSubmit}>
+        <div className="field-group">
+          <div className="field">
+            <label className="label">Name</label>
+            <input
+              className="input is-expanded"
+              value={name}
+              onChange={handleNameChange}
+            />
           </div>
 
+          <div className="field">
+            <label className="label">Cost</label>
+            <input
+              className="input is-expanded"
+              type="number"
+              value={cost || ''}
+              onChange={handleCostChange}
+            />
+          </div>
+        </div>
+        <div className="field">
+          <button className="button is-link">Submit</button>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default CarForm;
