f08481ff981c13f460346889383b6607072c7436
Rabia-Rajput <rajput.rabia@outlook.com>
2024-07-10 12:27:52 +0200

    update carform file

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeName } from '../store/slices';
+
 
 const CarForm = () => {
+  
+  const dispatch= useDispatch();
+ const name= useSelector((state)=>{
+    return state.form.name;
+  })
+  const handleChange=(e)=>{
+dispatch(changeName(e.target.value));
+  };
   return (
-    <div>CarForm</div>
+    <div className='car-form panel'>
+      <h4 className='subtitle is-3'>Add Car</h4>
+      <form>
+        <div className='field-group'> 
+          <div className='field'>
+            <label className='label'>Name</label>
+            <input className='input is-expanded' value={name} onChange={handleChange}/>
+          </div>
+
+        </div>
+      </form>
+    </div>
   )
 }
 
-export default CarForm
+export default CarForm;
